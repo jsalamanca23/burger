97e30f75aae414b8d10d40ebb00ae727d6cf97f7
Jose Salamanca <jsalamanca228@gmail.com>
2018-06-07 16:43:38 -0700

    added text

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('mc,chicken', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken farley bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('mc,double', true);
